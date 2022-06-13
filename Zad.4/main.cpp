216a4a8f4d72e7d3270966236ece45158418e3d4
KileverTM <leko0223@gmail.com>
2022-06-13 22:25:27 +0200

    V2 Stworzenie Pliku log z datą

--- Zad.4/main.cpp
+++ Zad.4/main.cpp
@@ -24,15 +24,63 @@ void createFile1(const std::string name, const int count, const char value)
 //01011111
 int main()
 {
+ofstream log;
+log.open("log.txt", ios_base::app);
+time_t czas;
+    struct tm * ptr;
+    time( & czas );
+    ptr = localtime( & czas );
+    char * data = asctime( ptr );
+
+log <<data<<"  Uruchomieie programu"<<endl;
+
+
+
+
 
 createFile1("test1_file1.bin", 100, 0x55);
+
+
+    time( & czas );
+    ptr = localtime( & czas );
+     data = asctime( ptr );
+    log<<data<<"  Stworzenie 1 pliku testowego"<<endl;
 createFile1("test1_file2.bin", 100, 0x55);
 
+    time( & czas );
+    ptr = localtime( & czas );
+    data = asctime( ptr );
+    log<<data<<"  Stworzenie 2 pliku testowego"<<endl;
+
 createFile1("test2_file1.bin", 100, 0x55);
+
+
+    time( & czas );
+    ptr = localtime( & czas );
+     data = asctime( ptr );
+    log<<data<<"  Stworzenie 3 pliku testowego"<<endl;
 createFile1("test2_file2.bin", 100, 0x5F);
 
+
+    time( & czas );
+    ptr = localtime( & czas );
+     data = asctime( ptr );
+    log<<data<<"  Stworzenie 4 pliku testowego"<<endl;
+
 createFile1("test3_file1.bin", 420000000, 0x55);
+
+
+    time( & czas );
+    ptr = localtime( & czas );
+     data = asctime( ptr );
+    log<<data<<"  Stworzenie 5 pliku testowego"<<endl;
 createFile1("test3_file2.bin", 420000000, 0x50);
+
+
+    time( & czas );
+    ptr = localtime( & czas );
+     data = asctime( ptr );
+    log<<data<<"  Stworzenie 6 pliku testowego"<<endl;
 
 
 
